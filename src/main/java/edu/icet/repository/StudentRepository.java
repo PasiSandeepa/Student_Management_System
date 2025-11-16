3ab93831b4ffb312f9b516dd8c52780a70eb95d7
PasiSandeepa <pasisandeepa456@gmail.com>
2025-11-16 11:18:22 +0530

    Update StudentRepository: use JpaRepository<StudentEntity, Integer>

---
+++ src/main/java/edu/icet/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package edu.icet.repository;
+
+import edu.icet.entity.StudentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface StudentRepository extends JpaRepository<StudentEntity,Integer> {
+
+
+}
